013bdbc78b96dd0544b3443b3e86ac5324b9fdc2
zakirovartem <79340389+zakirovartem@users.noreply.github.com>
2021-02-20 01:58:00 +0300

    Add files via upload

---
+++ logo.inc.php
@@ -0,0 +1,5 @@
+    
+                <div class="logo"> 
+                <img src="img/logo.jpg" alt="Aa">
+                </div>                                                 
+        
---
+++ menu.inc.php
@@ -0,0 +1,10 @@
+  
+            <nav>
+                <ul>
+                    <li><a href="#about_me"> Обо мне </a></li>
+                    <li><a href="#my_portfolio"> Мои достижения </a></li>
+                    <li><a href="#my_articles"> Мое любимое стихотворение </a></li>
+                    <li><a href="#links"> Ссылки на любимые ресурсы </a></li>
+                    <li><a href="#contacts"> Где меня найти </a></li>
+                </ul>    
+            </nav>
